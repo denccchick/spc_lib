50e1cd74760ff258d8ddaa2c8aac738a6b2bfea2
Denis <denisleonov20052@gmail.com>
2026-06-20 14:06:55 +0300

    feat: add capability calculation

--- spc_lib/core/base_chart.py
+++ spc_lib/core/base_chart.py
@@ -1,3 +1,5 @@
+# spc_lib/core/base_chart.py
+
 import numpy as np
 from typing import Optional, List
 
@@ -22,6 +24,7 @@ class BaseControlChart:
         self.lcl_disp = None
         self.main_label = "Основная статистика"
         self.disp_label = "Статистика разброса"
+        self.sigma_est = None
 
     def fit(self, baseline_mask=None, method='classic'):
         raise NotImplementedError
@@ -47,3 +50,64 @@ class BaseControlChart:
     def plot_rules(self, start=None, end=None, last_n=30, rules=None, n_cols=1, show_spec=False):
         from spc_lib.visualization import plot_rules_violations
         return plot_rules_violations(self, start, end, last_n, rules, n_cols, show_spec)
+
+    def capability(self, usl=None, lsl=None):
+            """
+            Расчет показателей пригодности процесса (Cp, Cpk, Cpl, Cpu).
+            """
+            usl = usl if usl is not None else self.usl
+            lsl = lsl if lsl is not None else self.lsl
+
+            if usl is None and lsl is None:
+                raise ValueError("Необходимо задать хотя бы одну границу спецификации (usl или lsl)")
+
+            if self.stat_main is None or self.cl_main is None:
+                raise ValueError("Карта не обучена (сначала вызовите .fit())")
+
+            # Центр процесса
+            mu = self.cl_main
+
+            # Оценка сигмы (внутригрупповой)
+            if self.sigma_est is not None:
+                sigma = self.sigma_est
+            elif self.ucl_main is not None and self.lcl_main is not None:
+                if self.subgroup_size > 1:
+                    sigma = (self.ucl_main - self.lcl_main) * np.sqrt(self.subgroup_size) / 6
+                else:
+                    sigma = (self.ucl_main - self.lcl_main) / 6
+            else:
+                # Fallback (общая дисперсия, даст Pp вместо Cp)
+                stats_data = np.asarray(self.stat_main)
+                stats_data = stats_data[~np.isnan(stats_data)]
+                sigma = np.std(stats_data, ddof=1)
+
+            if sigma <= 0:
+                return {'cp': np.nan, 'cpk': np.nan, 'cpl': np.nan, 'cpu': np.nan}
+
+            # Инициализируем NaN
+            cp = cpk = cpl = cpu = np.nan
+
+            # Считаем то, что возможно
+            if usl is not None and lsl is not None:
+                cp = (usl - lsl) / (6 * sigma)
+
+            if usl is not None:
+                cpu = (usl - mu) / (3 * sigma)
+
+            if lsl is not None:
+                cpl = (mu - lsl) / (3 * sigma)
+
+            # Cpk - это минимум из существующих односторонних индексов
+            if usl is not None and lsl is not None:
+                cpk = min(cpu, cpl)
+            elif usl is not None:
+                cpk = cpu
+            else:
+                cpk = cpl
+
+            return {
+                'cp': cp,
+                'cpk': cpk,
+                'cpl': cpl,
+                'cpu': cpu
+            }
